9230f301baf0d9a532dc2d12827658261da2b4d5
Taurean <taurean.dyer@gmail.com>
2019-02-26 23:47:03 +0000

    removed utils folder

--- utils/start-jupyter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -e "\n"
-echo "jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token=''"
-echo -e "\n"
-jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token=''
